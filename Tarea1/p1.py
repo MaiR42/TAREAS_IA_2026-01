2190ef1b5a821f14bc4d5f96d028ee00f3152e7f
Mair <163215578+MaiR42@users.noreply.github.com>
2026-04-18 01:39:35 -0400

    Add and analyze synthetic data to the dataset

--- Tarea1/p1.py
+++ Tarea1/p1.py
@@ -7,6 +7,7 @@ from pgmpy.estimators import BIC, BDeu, K2         # De estos solo se pide BIC p
 from pgmpy.models import DiscreteBayesianNetwork
 from pgmpy.estimators import MaximumLikelihoodEstimator
 from pgmpy.inference import VariableElimination
+from pgmpy.sampling import BayesianModelSampling
 
 
 import networkx as nx # Para ver grafos
@@ -39,7 +40,6 @@ print("========================================================================"
 
 
 # Cargar el dataset
-
 from ucimlrepo import fetch_ucirepo
 dataset = fetch_ucirepo(id=73) # Dataset: Mushroom
 X = dataset.data.features 
@@ -80,8 +80,8 @@ print("\n=== CONSTRUCCIÓN DE LA RED BAYESIANA (HILL CLIMBING) ===")
 
 # Que hace el algoritmo de hill climbing?
 
-# Encuentra una red bayesiana que se ajuste a los datos con una heuristica basada en un criterio de puntuacion (BIC),
-# lo que implica que no garantiza un optimo global, pero encuentra un optimo local en un buen tiempo de ejecucion
+# Va buscando soluciones de manera iterativa, evaluando el movimiento de nodos en base a un criterio de puntuación (BIC),
+#  lo que implica que no garantiza un óptimo global, pero encuentra un óptimo local en un tiempo de ejecución razonable
 
 initial_debug_time()
 
@@ -99,7 +99,7 @@ G = nx.DiGraph(modelo_hc.edges())
 plt.figure(figsize=(10, 8))
 pos = nx.spring_layout(G, seed=42, k=2) # Posiciona los nodos de forma estética, k=2 para la separacion entre nodos
 nx.draw(G, pos, with_labels=True, arrowsize=10, node_size=1000, font_size=12) 
-plt.title("DAG de la Red Bayesiana Aprendida (Mushroom)", size=15)
+plt.title("DAG de la Red Bayesiana Aprendida (Mushroom)", size=15, color='blue')
 plt.show()
 
 # Ajustar modelo para inferencia con Maximum Likelihood Estimation (MLE)
@@ -124,7 +124,7 @@ i2 = inferencia.query(
     evidence={'population': 'a'} # Poblacion abundante 
 )
 print("\n========= INFERENCIA 2=========")
-print("Probabilidad del habitad de un hongo \ndado a que su poblacion es abundante\n")
+print("Probabilidad del habitad de un hongo \ndado a que su poblacion es abundante (abundant)\n")
 print("P(habitad/population=abundante)")
 print("================================")
 print(i2)
@@ -132,10 +132,11 @@ print(i2)
 
 # # # # # # # # # # # # Construir red Bayesiana con Exhausive Search # # # # # # # # # # # # 
 
-# Se redujo drasticamente el numero de columnas del dataset para que el algoritmo de Exhaustive Search
-# pueda ejecutarse en un buen tiempo.
+# Explora todas las soluciones posibles del problema, las cuales luego son evaluadas con un sistema de puntuacion (BIC),
+# el hecho de que explore todo implica que encuentra un optimo global,
+# pero en un tiempo de ejecucion que crece de manera exponencial en relacion a la cantidad de nodos.
 
-# Dejando las variables escenciales para comprobar las inferencias anteriores. 
+# Se dejaron las variables escenciales para comprobar las inferencias anteriores. 
 # 5 variables por el momento (Con 6 el programa no termina de ejecutarse)
 # (En total se borraron 17 columnas (22->5))
 
@@ -161,14 +162,14 @@ final_debug_time()
 G = nx.DiGraph(modelo_ex.edges())
 plt.figure(figsize=(10, 8))
 pos = nx.spring_layout(G, seed=42, k=2) # Posiciona los nodos de forma estética, k=2 para la separacion entre nodos
-nx.draw(G, pos, with_labels=True, arrowsize=10, node_size=1000, font_size=12) 
+nx.draw(G, pos, with_labels=True, arrowsize=10, node_size=1000, font_size=12)
 plt.title("DAG de la Red Bayesiana Aprendida (Mushroom)", size=15)
 plt.show()
 
 # Ajustar modelo para inferencia con Maximum Likelihood Estimation (MLE)
 
 modelo2 = DiscreteBayesianNetwork(modelo_ex.edges())
-modelo2.fit(df, estimator=MaximumLikelihoodEstimator)
+modelo2.fit(df_short, estimator=MaximumLikelihoodEstimator)
 
 # Inferencias
 
@@ -189,10 +190,129 @@ i2 = inferencia.query(
     evidence={'population': 'a'} # Poblacion abundante 
 )
 print("\n========= INFERENCIA 2=========")
-print("Probabilidad del habitad de un hongo \ndado a que su poblacion es abundante\n")
+print("Probabilidad del habitad de un hongo \ndado a que su poblacion es abundante (abundant)\n")
 print("P(habitad/population=a)")
 print("================================")
 print(i2)
+
+# # # # # # # # # # # # # # # # # # # # # # # # Obtener datos sinteticos # # # # # # # # # # # # # # # # # # # # # # # # 
+
+# A partir del modelo con hill climbing
+sampler = BayesianModelSampling(modelo)
+
+n = len(df)
+
+data_10 = sampler.forward_sample(size=int(n * 0.10))
+df_10 = pd.concat([df, data_10], ignore_index=True)
+print(f"Dataset con 10% de datos sinteticos: {df_10.shape[0]} filas, {df_10.shape[1]} columnas")
+
+data_20 = sampler.forward_sample(size=int(n * 0.20))
+df_20 = pd.concat([df, data_20], ignore_index=True)
+print(f"Dataset con 20% de datos sinteticos: {df_20.shape[0]} filas, {df_20.shape[1]} columnas")
+
+data_40 = sampler.forward_sample(size=int(n * 0.40))
+df_40 = pd.concat([df, data_40], ignore_index=True)
+print(f"Dataset con 40% de datos sinteticos: {df_40.shape[0]} filas, {df_40.shape[1]} columnas")
+
+# # # # # # # # # Volver a construir la red bayesiana con los nuevos datasets # # # # # # # # #
+
+# Hill Climb
+
+hc_10 = HillClimbSearch(df_10)
+modelo_hc_10 = hc_10.estimate(scoring_method=BIC(df_10))
+print("Aristas encontradas (df_10):")
+print(modelo_hc_10.edges())
+modelo_10 = DiscreteBayesianNetwork(modelo_hc_10.edges())
+modelo_10.fit(df_10, estimator=MaximumLikelihoodEstimator)
+
+hc_20 = HillClimbSearch(df_20)
+modelo_hc_20 = hc_20.estimate(scoring_method=BIC(df_20))
+print("Aristas encontradas (df_20):")
+print(modelo_hc_20.edges())
+modelo_20 = DiscreteBayesianNetwork(modelo_hc_20.edges())
+modelo_20.fit(df_20, estimator=MaximumLikelihoodEstimator)
+
+hc_40 = HillClimbSearch(df_40)
+modelo_hc_40 = hc_40.estimate(scoring_method=BIC(df_40))
+print("Aristas encontradas (df_40):")
+print(modelo_hc_40.edges())
+modelo_40 = DiscreteBayesianNetwork(modelo_hc_40.edges())
+modelo_40.fit(df_40, estimator=MaximumLikelihoodEstimator)
+
+for i in range(3):
+    print(f"\n========= INFERENCIA 1 (df_{[10,20,40][i]}) =========")
+    inferencia = VariableElimination([modelo_10, modelo_20, modelo_40][i])
+    i1 = inferencia.query(
+        variables=['poisonous'],
+        evidence={'odor': 's'} # Olor spicy¿ (picante)
+    )
+    print("Probabilidad de que un hongo sea venenoso \ndado a que tiene olor picante (spicy)\n")
+    print(f"P(poisonous/odor=s) con df_{[10,20,40][i]}")
+    print("================================")
+    print(i1)
+    
+    print(f"\n========= INFERENCIA 2 (df_{[10,20,40][i]}) =========")
+    i2 = inferencia.query(
+    variables=['habitat'],
+    evidence={'population': 'a'} # Poblacion abundante 
+    )
+    print("Probabilidad del habitad de un hongo \ndado a que su poblacion es abundante (abundant)\n")
+    print(f"P(habitad/population=a) con df_{[10,20,40][i]}")
+    print("================================")
+    print(i2)
+
+
+
+# Exhaustive Search
+
+cols = ["poisonous", "odor", "cap-color", "habitat", "population"]
+
+df_10_short = df_10[cols]
+df_20_short = df_20[cols]
+df_40_short = df_40[cols]
+
+es_10 = ExhaustiveSearch(df_10_short, scoring_method=BIC(df_10_short))
+modelo_ex_10 = es_10.estimate()
+print("Aristas encontradas:")
+print(modelo_ex_10.edges())
+modelo2_10 = DiscreteBayesianNetwork(modelo_ex_10.edges())
+modelo2_10.fit(df_10, estimator=MaximumLikelihoodEstimator)
+
+es_20 = ExhaustiveSearch(df_20_short, scoring_method=BIC(df_20_short))
+modelo_ex_20 = es_20.estimate()
+print("Aristas encontradas:")
+print(modelo_ex_20.edges())
+modelo2_20 = DiscreteBayesianNetwork(modelo_ex_20.edges())
+modelo2_20.fit(df_20, estimator=MaximumLikelihoodEstimator)
+
+es_40 = ExhaustiveSearch(df_40_short, scoring_method=BIC(df_40_short))
+modelo_ex_40 = es_40.estimate()
+print("Aristas encontradas:")
+print(modelo_ex_40.edges())
+modelo2_40 = DiscreteBayesianNetwork(modelo_ex_40.edges())
+modelo2_40.fit(df_40, estimator=MaximumLikelihoodEstimator)
+
+for i in range(3):
+    print(f"\n========= INFERENCIA 1 (df_{[10,20,40][i]}_short) =========")
+    inferencia = VariableElimination([modelo2_10, modelo2_20, modelo2_40][i])
+    i1 = inferencia.query(
+        variables=['poisonous'],
+        evidence={'odor': 's'} # Olor spicy¿ (picante)
+    )
+    print("Probabilidad de que un hongo sea venenoso \ndado a que tiene olor picante (spicy)\n")
+    print(f"P(poisonous/odor=s) con df_{[10,20,40][i]}_short")
+    print("================================")
+    print(i1)
+    
+    print(f"\n========= INFERENCIA 2 (df_{[10,20,40][i]}_short) =========")
+    i2 = inferencia.query(
+    variables=['habitat'],
+    evidence={'population': 'a'} # Poblacion abundante 
+    )
+    print("Probabilidad del habitad de un hongo \ndado a que su poblacion es abundante (abundant)\n")
+    print(f"P(habitad/population=a) con df_{[10,20,40][i]}_short")
+    print("================================")
+    print(i2)
 
 ##################################################################################################
 tiempo_final = time.time()
