b9e05c7290a70783b5e1c3de10eaa4f32f4c6cf6
MaiR42 <mair.escobar@mail.udp.cl>
2026-06-05 18:54:26 -0400

    Cargar imagenes y definir resultado de mean-shift por parametros

--- Tarea2/tarea2.py
+++ Tarea2/tarea2.py
@@ -1,1 +1,18 @@
 # Inicio tarea 2
+import cv2
+
+img1 = cv2.imread("./data/img1.jpg")
+img2 = cv2.imread("./data/img2.jpg")
+img3 = cv2.imread("./data/img3.jpg")
+
+def resultado_imagen(p_img ,p_sp, p_sr, p_maxLevel):
+    resultado_img = cv2.pyrMeanShiftFiltering(
+        img=p_img,
+        sp=p_sp,
+        sr=p_sr,
+        p_maxLevel=0
+    )
+    return resultado_img
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
