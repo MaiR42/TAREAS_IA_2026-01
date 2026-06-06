acd60c2b256a413e5883e6bfa79b784837cd9dc5
MaiR42 <mair.escobar@mail.udp.cl>
2026-06-05 20:38:15 -0400

    Funcion para guardar las imagenes generadas por Mean Shift y funcion para comparar los resultados

--- Tarea2/tarea2.py
+++ Tarea2/tarea2.py
@@ -1,3 +1,5 @@
+GUARDAR_IMG = False # Para no demorarme tanton,,, DEBUG
+
 # Inicio tarea 2
 import cv2
 
@@ -14,8 +16,104 @@ def resultado_imagen(p_img ,p_sp, p_sr, p_maxLevel):
     )
     return resultado_img
 
-cv2.imshow("Original", img1)
-cv2.imshow("Mean Shift", resultado_imagen(img1, 15, 30, 0))
+# Pruebas
 
+imagenes = [img1, img2, img3]
+valores_sp = [5, 15, 30] # Elegir 3
+valores_sr = [10, 30, 60] # Elegir 3
+valores_maxLevel = [0,1]
+
+# Mostrar imagenes originales
+def mostrar_imagenes(imagenes):
+    i = 0
+    for img in imagenes:
+        i+=1
+        cv2.imshow(f"Original {i}", img)
+        input()
+
+if GUARDAR_IMG:
+    mostrar_imagenes(imagenes)
+
+# Guardar resultados
+import os
+
+os.makedirs("./resultados/img1", exist_ok=True)
+os.makedirs("./resultados/img2", exist_ok=True)
+os.makedirs("./resultados/img3", exist_ok=True)
+
+if GUARDAR_IMG:
+    for num_img, img in enumerate(imagenes, start=1):
+        for sp in valores_sp:
+            for sr in valores_sr:
+                for maxLevel in valores_maxLevel:
+                    resultado = resultado_imagen(
+                        img,
+                        sp,
+                        sr,
+                        maxLevel
+                    )
+
+                    nombre_archivo = (
+                        f"./resultados/img{num_img}/"
+                        f"sp{sp}_sr{sr}_max{maxLevel}.jpg"
+                    )
+
+                    cv2.imwrite(nombre_archivo, resultado)
+
+                    print("Guardado:", nombre_archivo)
+
+
+# Plot de imagenes para analisis
+import matplotlib.pyplot as plt
+
+def comparar_resultados(img, nombre_img, maxLevel):
+    fig, axes = plt.subplots(3, 3, figsize=(12, 12))
+    for i, sp in enumerate(valores_sp):
+        for j, sr in enumerate(valores_sr):
+            resultado = resultado_imagen(
+                img,
+                sp,
+                sr,
+                maxLevel
+            )
+
+            resultado_rgb = cv2.cvtColor(
+                resultado,
+                cv2.COLOR_BGR2RGB
+            )
+
+            axes[i, j].imshow(resultado_rgb)
+
+            axes[i, j].set_title(
+                f"sp={sp}, sr={sr}"
+            )
+
+            axes[i, j].axis("off")
+    plt.suptitle(
+        f"{nombre_img} - maxLevel={maxLevel}",
+        fontsize=16
+    )
+
+    plt.tight_layout()
+    plt.show()
+
+comparar_resultados(img1, "Imagen 1", 0)
+comparar_resultados(img1, "Imagen 1", 1)
+
+comparar_resultados(img2, "Imagen 2", 0)
+comparar_resultados(img2, "Imagen 2", 1)
+
+comparar_resultados(img3, "Imagen 3", 0)
+comparar_resultados(img3, "Imagen 3", 1)
+
+i = 0
+for img in imagenes:
+    i+=1
+    for v_maxLevel in valores_maxLevel:
+        comparar_resultados(img, f"Imagen {i}", v_maxLevel)
+
+
+
+# Nose q hace pero va al final
 cv2.waitKey(0)
 cv2.destroyAllWindows()
